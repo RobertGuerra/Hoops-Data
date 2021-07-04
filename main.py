96e390a46f4daec2ae54a2b3a2cb356be68a8754
csurob24 <robert5guerra@csu.fullerton.edu>
2021-07-03 22:40:15 -0700

    processed/cleaned dataframe, used pandas.join

--- main.py
+++ main.py
@@ -8,36 +8,43 @@ import dash
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import json
+import dash_table
 
-response = requests.get('https://www.balldontlie.io/api/v1/stats')
-
-print(response.text)
+response = requests.get('https://www.balldontlie.io/api/v1/players')
+res_json = json.loads(response.content)
+data = res_json["data"]
 
 # Initialize Application
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-balldontlie_df = pd.DataFrame.from_records(response.text)
+# json content into df
+temp_df = pd.DataFrame.from_records(data)
 
-def generateData(dataframe, col = 1000):
-    data = json.loads(response.text)
-    for i in data["data"]:
-        print(i)
+# separated player metadata
+player_metadata_df = temp_df.drop("team", axis=1) # axis=1 specifies column-wise drop
+player_metadata_df["player_id"] = player_metadata_df["id"]
+player_metadata_df = player_metadata_df.drop("id", axis=1)
 
+# separated team metadata
+team_df = pd.DataFrame.from_records(temp_df["team"])
+team_df["team_id"] = team_df["id"]
+team_df = team_df.drop("id", axis=1)
 
+# team_df["first_name"] = player_metadata_df["first_name"]
+
+ballislife_df = player_metadata_df.join(team_df)
+print("SPAGHETTI: ", ballislife_df[["full_name", "name", "abbreviation"]])
+print(ballislife_df.columns)
 
 # main app layout
-app.layout = html.Div([
-    html.H1('Game Time!'),
-    html.Div([
-        html.H1(children='Player Data',
-                className='table-heading'),
-
-        html.Div([
-            generateData(balldontlie_df)
-            ])
-        ]),
-    ])
-
+app.layout = dash_table.DataTable(ballislife_df,
+                             id = 'table',
+                             columns = [
+                                 dict(
+                                     name=col,
+                                     id=col
+                                 ) for col in ballislife_df.columns
+                             ])
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
