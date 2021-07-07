cc2705ff6e0b342968dab7d86a9cbddb7d8a9933
csurob24 <robert5guerra@csu.fullerton.edu>
2021-07-07 15:09:55 -0700

    updated title names and added animation to first place teams

--- card/create_container.py
+++ card/create_container.py
@@ -1,6 +1,7 @@
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
+
 def create_card(datum):
 
     # DATUM CONTIANS:
@@ -9,15 +10,18 @@ def create_card(datum):
     logo = datum['logo']
     record = datum['record']
     summary = datum['standingSummary']
-    color = datum['color']
+    team_color = datum['color']
     alt_color = datum['alternateColor']
 
-    # css class names
-    className="first-place" if "1st" in record else "not-first"
-    className="disp"
-    className="image"
 
-    print(name, record, summary,logo)
+    # fire for first place
+    numberOne = "fire" if "1st" in datum['standingSummary'] else ''
+
+
+    # give "white" to alt_color if it resembles team_color
+    if alt_color.upper() == team_color.upper() or name in ["Los Angeles Lakers", "Memphis Grizzlies", "Indiana Pacers", "Oklahoma City Thunder", "Toronto Raptors"]:
+        alt_color = "FFFFFF"
+
 
     card = dbc.Card(
             [
@@ -27,7 +31,17 @@ def create_card(datum):
                             [
                                 html.Div(
                                     [
-                                        html.H4(name, className="card-title", style={"display":"inline-table"}),
+                                        html.H4(
+                                            name,
+                                            className="card-title",
+                                            style=dict(
+                                                display="inline-table",
+                                                color="#" + team_color,
+                                                textShadow="-0.4px 0.6px #" + alt_color,
+                                                font="30px"
+                                            )
+                                        ),
+
                                         html.Img(
                                             src=f"{logo}",
                                             className="image-logo"
@@ -45,21 +59,27 @@ def create_card(datum):
                         fontFamily="Frutiger, Frutiger Linotype, Univers, Calibri, Gill Sans, Gill Sans MT, Myriad Pro, Myriad,\
                             DejaVu Sans Condensed, Liberation Sans, Nimbus Sans L, Tahoma, Geneva, Helvetica Neue, \
                             Helvetica, Arial, sans-serif",
-                        borderBottom="solid"
+                        #borderBottom="solid",
+                        backgroundColor="dark-gray",
+                        fontWeight="200"
                     )
                 ),
+
                 dbc.CardFooter(
                     [
                         html.Div(
                             [
                                 html.P(f"Season Record: {record}"),
-                                html.P(f"Standing: {summary}")
+                                html.P(f"Standing: {summary}",
+                                       className=numberOne)
                             ],
                         ),
                     ],
 
                     style=dict(
                         display="flex",
+                        backgroundColor='#' + team_color,
+                        justifyContent="center"
                     )
                 )
             ],
@@ -68,10 +88,8 @@ def create_card(datum):
             style=dict(
                 width="18rem",
                 margin="4px",
-                textAlign="-webkit-center"
+                textAlign="-webkit-center",
             ),
     )
-
-    print(type(card))
 
     return card
---
+++ codeSnippets.py
@@ -0,0 +1,7 @@
+
+
+# code for first place teams
+# firstPlace = "first_place" if "1st" in datum['standingSummary'] else ''
+
+
+
