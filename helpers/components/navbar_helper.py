9798353d1a0943e2b182ebbfb18a6f7fa60c1db4
RobertJG17 <robertjguerra@yahoo.com>
2021-07-22 20:28:22 -0700

    reverted to simple navbar config to keep responsive design

--- helpers/components/navbar_helper.py
+++ helpers/components/navbar_helper.py
@@ -3,87 +3,35 @@ import dash_bootstrap_components as dbc
 
 
 def Navbar():
+    nav = dbc.NavbarSimple(
+        children=[
+            html.H1(
+                "HOOPS DATA"
+            ),
 
-    nav = dbc.Nav(
-        [
-            dbc.Navbar(
-                children=[
-                    dbc.NavItem(
-                        dbc.NavLink(
-                            html.H1("NBA"),
-                            href="https://www.nba.com/",
-                            target="blank",
-                            className="nba",
-                        )
-                    ),
-                    dbc.NavItem(
-                        dbc.NavLink(
-                            html.H1(
-                                "HOOPS"
-                            ),
-                            style={"color": "white"}
-                        )
-                    ),
-                    dbc.NavItem(
-                        dbc.NavLink(
-                            dbc.DropdownMenu(
-                                [
-                                    dbc.DropdownMenuItem(i, id=i)
-                                    for i in sorted(
-                                    ["All Teams", "Atlantic", "Southeast", "Central",
-                                     "Northwest", "Pacific", "Southwest"]
-                                )
-                                ],
-
-                                nav=True,
-                                label="All Teams",
-                                id="team-list",
-                                in_navbar=True,
-                                className="dropdown",
-                            )
-                        )
-                    )],sticky=True, className="navbar"
+            dbc.DropdownMenu(
+                [
+                    dbc.DropdownMenuItem(i, id=i)
+                    for i in sorted(
+                        ["All Teams", "Atlantic", "Southeast", "Central", "Northwest", "Pacific", "Southwest"]
+                    )
+                ],
+                nav=True,
+                label="All Teams",
+                id="team-list",
+                in_navbar=True,
+                 className="dropdown"
             )
-
-            # dbc.NavItem(
-            #     dbc.NavLink(
-            #         html.H1("NBA"),
-            #         href="https://www.nba.com/",
-            #         target="blank",
-            #         className="nba",
-            #     )
-            # ),
-            # dbc.NavItem(
-            #     dbc.NavLink(
-            #         html.H1(
-            #             "HOOPS"
-            #         ),
-            #         style={"color":"white"}
-            #     )
-            # ),
-            # dbc.NavItem(
-            #     dbc.NavLink(
-            #         dbc.DropdownMenu(
-            #             [
-            #                 dbc.DropdownMenuItem(i, id=i)
-            #                 for i in sorted(
-            #                     ["All Teams", "Atlantic", "Southeast", "Central",
-            #                      "Northwest", "Pacific", "Southwest"]
-            #                 )
-            #             ],
-            #
-            #             nav=True,
-            #             label="All Teams",
-            #             id="team-list",
-            #             in_navbar=True,
-            #             className="dropdown",
-            #         )
-            #     )
-            # )
         ],
 
-        fill=True,
-        justified=True,
+        className="navbar",
+        brand="NBA",
+        fluid=True,
+        brand_href="https://www.nba.com/",
+        brand_external_link="https://www.nba.com/",
+        color="primary",
+        sticky=True,
+        dark=True
     )
 
     return nav
