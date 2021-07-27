8c5f17d4cd184b3ebfdca61d2f6328ff471d94a4
Roberto Guerra <15373732+RobertGuerra@users.noreply.github.com>
2021-07-27 14:30:53 -0700

    Update create_container.py

--- card/create_container.py
+++ card/create_container.py
@@ -33,9 +33,10 @@ def create_card(datum):
                                             src=f"{logo}",
                                             className="image-logo",
                                             style={
-                                                "opacity": ".4",
+#                                                 "opacity": ".4",
                                                 "z-index": "0",
-                                                "position": "relative"
+                                                "position": "relative",
+                                                "padding-bottom":"1opx"
                                             }
                                         )
                                     ],
