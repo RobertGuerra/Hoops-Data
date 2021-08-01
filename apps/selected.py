17125ad6a87f6618b6ecc5d09993872fca405147
Roberto <robert5guerra@gmail.com>
2021-07-31 18:17:20 -0700

    new branch for testing tab layout

--- apps/selected.py
+++ apps/selected.py
@@ -1,4 +1,6 @@
+from dash.exceptions import PreventUpdate
 import dash_html_components as html
+import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import pandas as pd
 
@@ -6,9 +8,8 @@ from dash.dependencies import Input, Output
 
 from card.init_data import json_data
 
-from helpers.create_stats_helper import create_stats
-
 from app import app
+import json
 
 layout = html.Div(
     [
@@ -18,11 +19,56 @@ layout = html.Div(
             id='stats-title-container'
 
         ),
-        html.Div(
 
-            className="stats-container",
-            id="stats-container"
+        # html.Div(
+        #
+        #     className="stats-container",
+        #     id="stats-container"
+        # ),
+
+        html.Div(
+            [
+                html.Hr(className="hr"),
+                html.H2(
+                    "Stats",
+                    style={
+                        "textAlignLast":"center"
+                    }
+                ),
+
+                html.Hr(className="hr"),
+
+
+                    html.Div(
+                        [
+                            dbc.Tabs(
+                                [
+                                    dbc.Tab(label="Playoff_seed", tab_id="tab-1"),
+                                    dbc.Tab(label="WINS", tab_id="tab-2"),
+                                    dbc.Tab(label="losses", tab_id="tab-3"),
+                                    dbc.Tab(label="winPercent", tab_id="tab-4"),
+                                    dbc.Tab(label="gamesBehind", tab_id="tab-5"),
+                                    dbc.Tab(label="gamesPlayed", tab_id="tab-6"),
+                                    dbc.Tab(label="pointsFor", tab_id="tab-7"),
+                                    dbc.Tab(label="pointsAgainst", tab_id="tab-8"),
+                                    dbc.Tab(label="avgPointsFor", tab_id="tab-9"),
+                                    dbc.Tab(label="avgPointsAgainst", tab_id="tab-10"),
+                                    dbc.Tab(label="divisionWinsPercent", tab_id="tab-11"),
+                                    dbc.Tab(label="leagueWinsPercent", tab_id="tab-12")
+                                ],
+                                id="tabs",
+                                active_tab="tab-1",
+                            ),
+                            html.Div(id="content"),
+                        ],
+                        style={"display":"flex", "flexDirection":"row"}
+                    )
+            ]
         ),
+
+        dcc.Store(
+            id="callback-store"
+        )
     ],
 
     className="stats-page-layout"
@@ -30,7 +76,7 @@ layout = html.Div(
 
 
 @app.callback([Output('stats-title-container', 'children'),
-               Output('stats-container', 'children'),],
+               Output('callback-store', 'data')],
               [Input('url', 'pathname')])
 def fetch_stats(pathname):
     name = pathname.split('/')[3].replace('%20', ' ')
@@ -40,7 +86,7 @@ def fetch_stats(pathname):
     df = pd.DataFrame.from_records(data)
 
 
-    stats_children = create_stats(df)
+    # stats_children = create_stats(df)
 
 
     team_color = df['color']
@@ -88,5 +134,155 @@ def fetch_stats(pathname):
         ),
     ]
 
+    data = json.dumps(df.to_json())
 
-    return title_children, stats_children
+
+    return title_children, data
+
+@app.callback(Output("content", "children"),
+              [Input("callback-store", "data"),
+               Input("tabs", "active_tab")])
+def switch_tab(data, at):
+
+    if data is None:
+        raise PreventUpdate
+
+    df = pd.read_json(json.loads(data))
+
+    stats_rec = df["record"][0]['items'][0]['stats']
+    stats_rec_formatted = [{d['name']: d['value']} for d in stats_rec]
+
+    stats_ser = pd.Series(index=[list(d.keys())[0] for d in stats_rec_formatted],
+                          data=[list(d.values())[0] for d in stats_rec_formatted])
+
+
+    tab1_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{}".format(int(stats_ser['playoffSeed'])))
+            ],
+
+            style = {"textAlignLast": "left", "width":"100%", "height":"100%"}
+        )
+        #className="mt-3",
+    )
+
+    tab2_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{}".format(int(stats_ser['wins'])))
+            ],
+            style={"textAlignLast":"left"}
+        ),
+    )
+    tab3_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{}".format(int(stats_ser['losses'])))
+            ],
+            style={"textAlignLast": "left"}
+        ),
+    )
+    tab4_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{:.2%}".format(stats_ser['winPercent']))
+            ],
+            style={"textAlignLast": "left"}
+        ),
+    )
+    tab5_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{}".format(int(stats_ser['gamesBehind'])))
+            ],
+            style={"textAlignLast": "left"}
+        ),
+    )
+    tab6_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{}".format(int(stats_ser['gamesPlayed'])))
+            ],
+            style={"textAlignLast": "left"}
+        ),
+    )
+    tab7_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{}".format(int(stats_ser['pointsFor'])))
+            ],
+            style={"textAlignLast": "left"}
+        ),
+    )
+    tab8_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{}".format(int(stats_ser['pointsAgainst'])))
+            ],
+            style={"textAlignLast":"left"}
+        ),
+    ),
+    tab9_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{}".format(int(stats_ser['avgPointsFor'])))
+            ],
+            style={"textAlignLast": "left"}
+        ),
+    ),
+    tab10_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{}".format(int(stats_ser['avgPointsAgainst'])))
+            ],
+            style={"textAlignLast": "left"}
+        ),
+    ),
+    tab11_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{:.2%}".format(stats_ser['divisionWinPercent']))
+            ],
+            style={"textAlignLast": "left"}
+        ),
+    ),
+    tab12_content = dbc.Card(
+        dbc.CardBody(
+            [
+                html.P("{:.2%}".format(stats_ser['leagueWinPercent']))
+            ],
+            style={"textAlignLast": "left"}
+        ),
+    ),
+
+
+
+    if at == "tab-1":
+        return tab1_content
+    elif at == "tab-2":
+        return tab2_content
+    elif at == "tab-3":
+        return tab3_content
+    elif at == "tab-4":
+        return tab4_content
+    elif at == "tab-5":
+        return tab5_content
+    elif at == "tab-6":
+        return tab6_content
+    elif at == "tab-7":
+        return tab7_content
+    elif at == "tab-8":
+        return tab8_content
+    elif at == "tab-9":
+        return tab9_content
+    elif at == "tab-10":
+        return tab10_content
+    elif at == "tab-11":
+        return tab11_content
+    elif at == "tab-12":
+        return tab12_content
+    return html.P("This shouldn't ever be displayed...")
+
+
+
--- helpers/create_stats_helper.py
+++ helpers/create_stats_helper.py
@@ -1,104 +1,106 @@
+import dash.exceptions
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
 import pandas as pd
+import json
+from app import app
 
 
-def create_bubble(title, info):
-    return dbc.Card(
-        dbc.CardBody(
-            [
-                html.H5(title, style={"text-align-last": "center"}),
-                html.Hr(style={"backgroundColor": "white"}),
-                html.P(round(info, 2), style={"text-align-last": "center"})
-            ]
-        ),
-
-        style={"border-radius": "24px"}
-    )
 
 
-def create_stats(df):
-    home = df["record"][0]['items'][0]['stats']
-    home = [{d['name']: d['value']} for d in home]
+
+
+
+# def create_bubble(title, info):
+#     return dbc.Card(
+#         dbc.CardBody(
+#             [
+#                 html.H5(title, style={"text-align-last": "center"}),
+#                 html.Hr(style={"backgroundColor": "white"}),
+#                 html.P(round(info, 2), style={"text-align-last": "center"})
+#             ]
+#         ),
+#
+#         style={"border-radius": "24px"}
+#     )
+#
+# def create_stats(df):
+#     home = df["record"][0]['items'][0]['stats']
+#     home = [{d['name']: d['value']} for d in home]
 
     # away = df["record"][0]['items'][2]['stats']
     # away = [{d['name']: d['value']} for d in away]
 
-    home_ser = pd.Series(index=[list(d.keys())[0] for d in home], data=[list(d.values())[0] for d in home])
+    # home_ser = pd.Series(index=[list(d.keys())[0] for d in home], data=[list(d.values())[0] for d in home])
     # away_ser = pd.Series(index=[list(d.keys())[0] for d in away], data=[list(d.values())[0] for d in away])
 
 
 
-    home_div = html.Div(
-        [
-            html.Hr(className="hr"),
-            html.H2(
-                "Stats",
-                style={
-                    "textAlignLast":"center"
-                }
-            ),
+    # home_div =
 
-            html.Hr(className="hr"),
 
-            html.Div(
-                [
-                    create_bubble(title="Playoff Seed", info=home_ser['playoffSeed']),
-                    create_bubble(title="Wins", info=home_ser['wins']),
-                    create_bubble(title="Losses", info=home_ser['losses']),
-                    create_bubble(title="Win %", info=home_ser['winPercent']),
-                    create_bubble(title="Games Behind", info=home_ser['gamesBehind']),
-                    create_bubble(title="Games Played", info=home_ser['gamesPlayed']),
-                    create_bubble(title="Points Scored", info=home_ser['pointsFor']),
-                    create_bubble(title="Points Against", info=home_ser['pointsAgainst']),
-                    create_bubble(title="Avg. Points Scored", info=home_ser['avgPointsFor']),
-                    create_bubble(title="Avg. Points Against", info=home_ser['avgPointsAgainst']),
-                    create_bubble(title="Division Win %", info=home_ser['divisionWinPercent']),
-                    create_bubble(title="League Win %", info=home_ser['leagueWinPercent'])
-                ],
-
-                className="bubble-grid"
-                # html.P(
-                #     "Playoff_seed: {}".format(int(home_ser['playoffSeed']))
-                # ),
-                # html.P(
-                #     "WINS: {}".format(int(home_ser['wins']))
-                # ),
-                # html.P(
-                #     "LOSSES: {}".format(int(home_ser['losses']))
-                # ),
-                # html.P(
-                #     "win%: {:.2%}".format((home_ser['winPercent']))
-                # ),
-                # html.P(
-                #     "Games_behind: {}".format(int(home_ser['gamesBehind']))
-                # ),
-                # html.P(
-                #     "Games_Played: {}".format(int(home_ser['gamesPlayed']))
-                # ),
-                # html.P(
-                #     "Points_For: {}".format(int(home_ser['pointsFor']))
-                # ),
-                # html.P(
-                #     "Points_Against: {}".format(int(home_ser['pointsAgainst']))
-                # ),
-                # html.P(
-                #     "avg_Points_For: {}".format(int(home_ser['avgPointsFor']))
-                # ),
-                # html.P(
-                #     "avg_Points_Against: {}".format(int(home_ser['avgPointsAgainst']))
-                # ),
-                # html.P(
-                #     "division_Win_Percent: {:.2%}".format((home_ser['divisionWinPercent']))
-                # ),
-                # html.P(
-                #     "league_Win_Percent: {:.2%}".format((home_ser['leagueWinPercent']))
-                # )
-            )
-        ],
-
-        style={'width': '100%'}
-    )
+            # html.Div(
+            #
+            #     [
+            #         create_bubble(title="Playoff Seed", info=home_ser['playoffSeed']),
+            #         create_bubble(title="Wins", info=home_ser['wins']),
+            #         create_bubble(title="Losses", info=home_ser['losses']),
+            #         create_bubble(title="Win %", info=home_ser['winPercent']),
+            #         create_bubble(title="Games Behind", info=home_ser['gamesBehind']),
+            #         create_bubble(title="Games Played", info=home_ser['gamesPlayed']),
+            #         create_bubble(title="Points Scored", info=home_ser['pointsFor']),
+            #         create_bubble(title="Points Against", info=home_ser['pointsAgainst']),
+            #         create_bubble(title="Avg. Points Scored", info=home_ser['avgPointsFor']),
+            #         create_bubble(title="Avg. Points Against", info=home_ser['avgPointsAgainst']),
+            #         create_bubble(title="Division Win %", info=home_ser['divisionWinPercent']),
+            #         create_bubble(title="League Win %", info=home_ser['leagueWinPercent'])
+            #     ],
+            #
+            #     className="bubble-grid"
+            #
+            #     # html.P(
+            #     #     "Playoff_seed: {}".format(int(home_ser['playoffSeed']))
+            #     # ),
+            #     # html.P(
+            #     #     "WINS: {}".format(int(home_ser['wins']))
+            #     # ),
+            #     # html.P(
+            #     #     "LOSSES: {}".format(int(home_ser['losses']))
+            #     # ),
+            #     # html.P(
+            #     #     "win%: {:.2%}".format((home_ser['winPercent']))
+            #     # ),
+            #     # html.P(
+            #     #     "Games_behind: {}".format(int(home_ser['gamesBehind']))
+            #     # ),
+            #     # html.P(
+            #     #     "Games_Played: {}".format(int(home_ser['gamesPlayed']))
+            #     # ),
+            #     # html.P(
+            #     #     "Points_For: {}".format(int(home_ser['pointsFor']))
+            #     # ),
+            #     # html.P(
+            #     #     "Points_Against: {}".format(int(home_ser['pointsAgainst']))
+            #     # ),
+            #     # html.P(
+            #     #     "avg_Points_For: {}".format(int(home_ser['avgPointsFor']))
+            #     # ),
+            #     # html.P(
+            #     #     "avg_Points_Against: {}".format(int(home_ser['avgPointsAgainst']))
+            #     # ),
+            #     # html.P(
+            #     #     "division_Win_Percent: {:.2%}".format((home_ser['divisionWinPercent']))
+            #     # ),
+            #     # html.P(
+            #     #     "league_Win_Percent: {:.2%}".format((home_ser['leagueWinPercent']))
+            #     # )
+            # )
+    #     ],
+    #
+    #     style={'width': '100%'}
+    # )
 
     # away_div = html.Div(
     #     [
@@ -121,4 +123,4 @@ def create_stats(df):
     #     style={'width': '100%'}
     # )
 
-    return [home_div]#, away_div]
+    # return [home_div], away_div]
